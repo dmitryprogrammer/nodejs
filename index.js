3dd0cbda2879ac367724473c7111db19b35ce31d
dpischalka <pischalkads@altarix.ru>
2018-09-13 18:54:48 +0400

    first lessons.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const server = require("./server");
 const db = require("./db");
+const logger = require("./logger")(module);
 
 db.connect();
-db.getPhrase("Run successful");
+logger(db.getPhrase("Run successful"));
 
 server.server("test");
