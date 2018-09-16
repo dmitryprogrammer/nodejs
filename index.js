c953e8d2fb5db454a78637897af942cc781574fc
PischalkaDS <pischalkads@altarix.ru>
2018-09-16 16:57:29 +0400

    remove unnecessary code.

--- index.js
+++ index.js
@@ -1,31 +1,9 @@
-const server = require("./server");
-const db = require("./db");
-const util = require("util");
-const logger = require("./logger")(module);
-const PhraseError = require("./errors").PhraseError;
-const HttpError = require("./errors").HttpError;
+const http = require("http");
 
-db.connect();
-db.logPhrase("Run successful");
-
-function getPhrase(name) {
-    const receivedPhrase = db.getPhrase(name);
-    if (!receivedPhrase) {
-        throw new PhraseError(`Нет такой фразы: ${name}`);
-    }
-    return receivedPhrase;
-}
-
-function makePage(url) {
-    if (url !== "index.html") {
-        throw new HttpError(404, `Нет такой страницы: ${url}`);
-    }
-    return util.format(`%s, %s!`, getPhrase("Hell"), getPhrase("Run successful"));
-}
-
-try {
-    const page = makePage("index.htm");
-    console.log(page);
-} catch (e) {
-    console.error(`Ошибка %s\n сообщение: %s\n стек: %s`, e.name, e.message, e.stack);
-}
+/**
+ * Creation web-server
+ */
+http.createServer((req, res) => {
+    res.writeHead(200, {"Content-type": "text/html"});
+    res.end("<h1 style='text-align: center;display:block;'>Hello World!</h1>");
+}).listen(8080);
