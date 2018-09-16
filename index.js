831ed88c62d6135297e98208c291cc48baeea5a7
PischalkaDS <pischalkads@altarix.ru>
2018-09-16 16:58:40 +0400

    trying including lodash.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const http = require("http");
-
+const _ = require("lodash");
 /**
  * Creation web-server
  */
 http.createServer((req, res) => {
-    res.writeHead(200, {"Content-type": "text/html"});
-    res.end("<h1 style='text-align: center;display:block;'>Hello World!</h1>");
+    if (!_.isUndefined(res)) {
+        res.writeHead(200, {"Content-type": "text/html"});
+        res.end("<h1 style='text-align: center;display:block;'>Hello World!</h1>");
+    }
 }).listen(8080);
