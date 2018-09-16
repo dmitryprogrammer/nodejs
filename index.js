40eb72e4b3a8021498c8abeae7db9431f248b54d
PischalkaDS <pischalkads@altarix.ru>
2018-09-16 17:36:59 +0400

    rewriting server on eventing model.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const http = require("http");
 const _ = require("lodash");
+const server = new http.Server;
 /**
  * Creation web-server
  */
-http.createServer((req, res) => {
+server.listen(8080);
+server.on("request", (req, res) => {
     if (!_.isUndefined(res)) {
         res.writeHead(200, {"Content-type": "text/html"});
         res.end("<h1 style='text-align: center;display:block;'>Hello World!</h1>");
     }
-}).listen(8080);
+});
