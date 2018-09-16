bb221d76425a9407187e364c39d8e485de461640
PischalkaDS <pischalkads@altarix.ru>
2018-09-16 20:38:06 +0400

    echo server.

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const http = require("http");
+const url = require("url");
 const _ = require("lodash");
-const server = new http.Server;
+const server = new http.Server();
 /**
  * Creation web-server
  */
-server.listen(8080);
 server.on("request", (req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+    console.log(parsedUrl.query);
+
     if (!_.isUndefined(res)) {
         res.writeHead(200, {"Content-type": "text/html"});
+        res.write("<h1 style='text-align: center;display:block;'>" + parsedUrl.query.message + "</h1>");
+        res.write("<h1 style='text-align: center;display:block;'>Hello World!</h1>");
         res.end("<h1 style='text-align: center;display:block;'>Hello World!</h1>");
     }
 });
+server.listen(8080);
