4d2173ebb37204592da35ba641ce751eb592bd7c
dpischalka <dpischalka@gmail.com>
2018-11-24 17:21:25 +0400

    practicing in http

--- index.js
+++ index.js
@@ -1,33 +1,18 @@
 const http = require("http");
-const url = require("url");
-const server = new http.Server();
-const _ = require("lodash");
-/**
- * Creation web-server
- */
-server.on("request", (req, res) => {
-    const parsedUrl = url.parse(req.url, true);
 
-    if (!_.isUndefined(res)) {
-        res.writeHead(200, {"Content-type": "text/html"});
-        res.write("<h1 style='text-align: center;display:block;'>" + parsedUrl.query["message"] + "</h1>");
-        res.write("<h1 style='text-align: center;display:block;'>Hello World!</h1>");
-        res.end("<h1 style='text-align: center;display:block;'>Hello World!</h1>");
-    }
+const server = http.createServer();
+
+server.on("request", function(req, res) {
+  res.writeHead(200, {"Content-Type": "text/html"});
+  res.end("<h1>Hello World!</h1>");
+  console.log("request");
 });
-server.listen(8080);
 
-process.stdin.setEncoding("utf8");
+server.on("connection", function(req, res) {
+  console.log("connection", req.localPort);
+});
 
-process.stdin.on("readable", () => {
-    const input = process.stdin.read();
-
-    if (input !== null) {
-        process.stdout.write(input);
-    }
-
-    const command = input.trim();
-    if (command === "exit") {
-        process.exit(0);
-    }
+server.listen(4200);
+server.on("listening", () => {
+  console.log("listening");
 });
