77022198c95ef75911d98ceb4f2e976a519a643c
dpischalka <dpischalka@gmail.com>
2018-11-25 10:10:03 +0400

    practicing with createWritableStream

--- index.js
+++ index.js
@@ -1,18 +1,49 @@
-const http = require("http");
+const util = require("util");
+const ee = require("events").EventEmitter;
+const fs = require("fs");
 
-const server = http.createServer();
+function InputChecker(name, file) {
+  this.name = name;
+  this.writeStream = fs.createWriteStream(`./${file}.txt`, {flags: "a", encoding: "utf8", mode: 0o666});
+}
 
-server.on("request", function(req, res) {
-  res.writeHead(200, {"Content-Type": "text/html"});
-  res.end("<h1>Hello World!</h1>");
-  console.log("request");
+util.inherits(InputChecker, ee);
+
+InputChecker.prototype.check = function(i) {
+  const input = i.trim();
+  const command = input.substr(0, 3);
+  const message = input.substr(3, input.length);
+
+  switch (command) {
+    case "wr:":
+      this.emit("write", `${message}\n`);
+      break;
+    case "en:":
+      this.emit("end");
+      break;
+    default:
+      this.emit("echo", input);
+  }
+};
+
+const ic = new InputChecker("My first test", "output");
+
+ic.on("write", function(data) {
+  this.writeStream.write(data, "utf8");
 });
 
-server.on("connection", function(req, res) {
-  console.log("connection", req.localPort);
+ic.on("echo", function(data) {
+  process.stdout.write(`${ic.name}wrote${data}`);
 });
 
-server.listen(4200);
-server.on("listening", () => {
-  console.log("listening");
+ic.on("end", function() {
+  process.exit();
+});
+
+process.stdin.setEncoding("utf8");
+process.stdin.on("readable", function() {
+  let input = process.stdin.read();
+  if (input !== null) {
+    ic.check(input);
+  }
 });
