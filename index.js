55b4c8cd1f427bf38d7e2eb7d0b3dc96586eb282
dpischalka <pischalkads@altarix.ru>
2018-12-24 18:04:01 +0400

    practicing in http

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
-const fs = require("fs");
-const test = require("./db/test");
-const console = require("console").Console;
-const logStream = fs.createWriteStream("./db/log.txt");
-const errorStream = fs.createWriteStream("./db/err.txt");
-const logger = new console(logStream, errorStream);
+const {EventEmitter} = require("events");
+const ee = new EventEmitter;
 
-function readFiles() {
-    fs.readdir("./db", (err, dir) => {
-        logger.log(dir);
-        dir.forEach(file => {
-            logger.log(file);
-            fs.readFile(file, (errF, file) => {
-                if (!errF) {
-                    logger.log(file ? file.toString() : file);
-                }
-            });
-        });
-    });
-}
+ee.on("a", (data) => {
+    console.log(data);
+});
 
-readFiles();
+ee.emit("a", {test: "test"});
