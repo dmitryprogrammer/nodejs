e0c19eba4336733ae14e4cd2ada3f79f99d3dfcf
dpischalka <dpischalka@gmail.com>
2019-02-21 00:15:12 +0400

    practice express.

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
+app.get("/test", (req, res) => {
+  console.log(req);
+  console.log(res);
+  res.send("test");
+});
+
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
 });
